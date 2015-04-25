3a12dd91f024eab13987e3e294243e16f5ab5417
Tyler Black <blacktylera@gmail.com>
2015-04-25 18:38:27 -0500

    Pushing month name and year into string. Starting logic for accurate day printing and layout.

---
+++ lib/month.rb
@@ -0,0 +1,35 @@
+class Month
+  attr_reader :month, :year
+
+def initialize(month, year)
+   @month = month
+   @year = year
+end
+
+
+def name
+	month_name_hash = {1 => "January", 2 => "Feburary", 3 => "March", 4 => "April", 5 => "May", 6 => "June",
+	7 => "July", 8 => "August", 9 => "Septemnber", 10 => "October", 11 => "November", 12 => "December"}
+	return month_name_hash[@month]
+end
+
+def day_count
+	day_count_hash = {1 => 31, 2 => 28, 3 => 31, 4 => 30, 5 => 31, 6 => 30, 7 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31}
+  return day_count_hash[@month]
+end
+
+def to_s
+  cal_string = ""
+  width = 20
+	month_name_year_head = "#{name} #{@year}".center(width).rstrip
+	weekday_name_head = "Su Mo Tu We Th Fr Sa"
+
+	cal_string << month_name_year_head
+	cal_string << "\n"
+	cal_string << weekday_name_head
+
+	
+
+
+end
+end
---
+++ test/test_month.rb
@@ -0,0 +1,65 @@
+require_relative 'helper'
+require_relative '../lib/month'
+
+class TestMonth < Minitest::Test
+  def test_initializing_a_month_saves_values
+    m = Month.new(05, 2015)
+    assert_equal 05, m.month
+    assert_equal 2015, m.year
+  end
+
+  def test_to_s_on_jan_2012
+    m = Month.new(01, 2012)
+    expected = <<EOS
+    January 2012
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
+EOS
+    assert_equal expected, m.to_s
+  end
+
+  def test_to_s_on_jan_2017
+    m = Month.new(01, 2017)
+    expected = <<EOS
+    January 2017
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
+EOS
+    assert_equal expected, m.to_s
+  end
+
+  def test_check_non_leap_year
+    m = Month.new(02, 2015)
+    expected = 28
+    assert_equal expected, m.non_leap_year
+  end
+
+  def test_leap_year
+    m = Month.new(02, 2000)
+    expected = 29
+    assert_equal expected, m.leap_year
+  end
+end
+
+  def test_march_days
+    m = Month.new(03, 2000)
+    expected = 31
+    assert_equal expected, m.check_for_days
+  end
+
+  def test_name_of_month
+   m = Month.new(07, 2013)
+   expected = 'July'
+   assert_equal expected, m.name
+  end
+
+
+

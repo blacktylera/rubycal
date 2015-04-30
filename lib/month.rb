8965d754cdb654aee20352798225188e5cdcdfa0
Tyler Black <blacktylera@gmail.com>
2015-04-29 19:46:42 -0500

    Month edit and test edit.

--- lib/month.rb
+++ lib/month.rb
@@ -9,7 +9,6 @@ def initialize(month, year)
    @year = year
 end
 
-
 def name
 	month_name_hash = {1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June",
 	7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December"}
